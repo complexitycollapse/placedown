edda4a38a5653d7d7093a214b987fbc47afa86ab
complexitycollapse <complexitycollapse@protonmail.com>
2024-09-22 19:06:44 +0100

    Make heading changeable

--- components/cache-list.jsx
+++ components/cache-list.jsx
@@ -2,9 +2,13 @@ import getCache from "../auxiliary/cache";
 import TreeComponent from "./tree-component";
 
 export default function CacheList() {
-  return (<TreeComponent
-    treeData={setNodeTreeData => getCache().filenames().then(names => setNodeTreeData(names.map(makeNode)))}>
-  </TreeComponent>);
+  return (
+  <div>
+    <h1>Leaf Cache</h1>
+    <TreeComponent
+      treeData={setNodeTreeData => getCache().filenames().then(names => setNodeTreeData(names.map(makeNode)))}>
+    </TreeComponent>
+  </div>);
 }
 
 function makeNode(name) {
