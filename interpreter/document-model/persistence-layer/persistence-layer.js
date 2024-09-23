cadf477e4705c388a540376ab2e2e0f7d33e0e60
complexitycollapse <complexitycollapse@protonmail.com>
2024-09-23 01:22:52 +0100

    Raise event when persister added

--- interpreter/document-model/persistence-layer/persistence-layer.js
+++ interpreter/document-model/persistence-layer/persistence-layer.js
@@ -1,9 +1,11 @@
 import ContentPersister from "./content-persister";
 import getCache from "../../../auxiliary/cache";
+import { removeItem } from "../../../common/utils";
 
 export default function PersistenceLayer() {
 
   let unique = 0;
+  const subscriptions = [];
 
   const obj = {
     objects: [],
@@ -11,12 +13,17 @@ export default function PersistenceLayer() {
       let persister = undefined;
       if (pointer.isContent) {
         persister = ContentPersister((++unique).toString(), pointer);
+      } else {
+        throw new Error("Unsupported pointer: ", JSON.stringify(pointer));
       }
 
       obj.objects.push(persister);
+      for (const callback of subscriptions) { callback(); }
 
-      getCache(pointer.origin).then(content => persister.setValue(content)).catch(reason => console.error(reason));
-    }
+      getCache().get(pointer.origin).then(content => persister.setValue(content)).catch(reason => console.error(reason));
+    },
+    subscribeToAdd: callback => subscriptions.push(callback),
+    unsubscribeToAdd: callback => removeItem(subscriptions, callback)
   };
 
   return obj;
