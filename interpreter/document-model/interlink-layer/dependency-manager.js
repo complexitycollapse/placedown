a7619659a45a5694522769b137a32dd41df33613
complexitycollapse <complexitycollapse@protonmail.com>
2024-10-01 23:17:45 +0100

    WorkManager

--- interpreter/document-model/interlink-layer/dependency-manager.js
+++ interpreter/document-model/interlink-layer/dependency-manager.js
@@ -1,26 +1,11 @@
-import { ListMap } from "../../../common/utils";
-
-export default function DependencyManager() {
+export default function WorkManager() {
   const obj = {
-    newWork: new ListMap(),
-    byPointer: new ListMap(),
-    add: (key, pointer, ilink, payload) => {
-      const job = {key, pointer, ilink, payload};
-      obj.newWork.push(pointer.hashableName, job);
-      obj.byPointer.push(pointer.hashableName, job);
-      ilink.dependencies.push(key, job);
+    newWork: new Map(),
+    meshpoints: new Set(),
+    requestLoad: (pointer) => {
+      newWork.set(pointer.hashableName, pointer);
     },
-    getForPointer: pointer => obj.byPointer.get(pointer.hashableName),
-    fulfil: (pointer) => {
-      obj.newWork.remove(pointer.hashableName);
-      const jobs = obj.byPointer.get(pointer.hashableName);
-      obj.byPointer.remove(pointer.hashableName);
-      jobs.forEach(job => {
-        ilink.dependencies.removeItem(job);
-      });
-
-      return jobs;
-    }
+    notifyMeshpointReady: meshpoint => meshpoints.add(meshpoint)
   };
 
   return obj;
