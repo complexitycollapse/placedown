3a6aa3cd0989f7a00e8167e90c9cea3e24d155bd
complexitycollapse <complexitycollapse@protonmail.com>
2024-10-20 10:22:58 +0100

    Identifying classes and attributes

---
+++ interpreter/document-model/object-constants.js
@@ -0,0 +1,4 @@
+import LinkPointer from "../../data-model/link-pointer";
+
+export const attributeType = LinkPointer("attribute-link.json");
+export const classType = LinkPointer("class-link.json");
--- interpreter/document-model/semantic-layer/semantic-layer.js
+++ interpreter/document-model/semantic-layer/semantic-layer.js
@@ -1,12 +1,15 @@
 import Layer from "../layer";
 import SemanticNode from "./semantic-node";
+import { attributeType, classType } from "../object-constants";
+import { getOrSet } from "../../../common/utils";
 
 export default function SemanticLayer() {
   const obj = Layer();
   
   Object.assign(obj, {
     elements: [],
-    classes: [],
+    classes: new Map(),
+    attributes: new Map(),
     onMeshpointsReady: meshpoints => {
       meshpoints.forEach(createNode);
       obj.notify();
@@ -14,8 +17,37 @@ export default function SemanticLayer() {
   });
 
   function createNode(meshpoint) {
-    obj.elements.push(SemanticNode(meshpoint));
+    const node = SemanticNode(meshpoint);
+    obj.elements.push(node);
+
+    if (!node.leafType === "link") {
+      return;
+    }
+
+    if (attributeType.denotesSame(node.type)) {
+      getOrSet(obj.attributes, node.key, () => Attribute(node));
+    }
+
+    if (classType.denotesSame(node.type)) {
+      getOrSet(obj.classes, node.key, () => Class(node));
+    }
   }
+
+  return obj;
+}
+
+function Attribute(node) {
+  let obj = {
+    node
+  };
+
+  return obj;
+}
+
+function Class(node) {
+  let obj = {
+    node
+  };
 
   return obj;
 }
