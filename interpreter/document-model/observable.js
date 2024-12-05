54a5f333b813d35a66f12e509466c25699ca1e80
complexitycollapse <complexitycollapse@protonmail.com>
2024-12-05 11:50:56 +0000

    Spelling fix

--- interpreter/document-model/observable.js
+++ interpreter/document-model/observable.js
@@ -1,10 +1,10 @@
 import { removeItem } from "../../common/utils";
 
 export default function makeObservable(obj) {
-  obj.dependants = [];
-  obj.subscribe = callback => obj.dependants.push(callback);
-  obj.unsubscribe = callback => removeItem(obj.dependants, callback);
-  obj.notify = () => obj.dependants.forEach(callback => callback(obj));
+  obj.dependents = [];
+  obj.subscribe = callback => obj.dependents.push(callback);
+  obj.unsubscribe = callback => removeItem(obj.dependents, callback);
+  obj.notify = () => obj.dependents.forEach(callback => callback(obj));
 
   return obj;
 }
